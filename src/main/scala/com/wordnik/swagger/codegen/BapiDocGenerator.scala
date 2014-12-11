2f1ca4bc964e861e81875cd264f025a8e8c310ff
Justin Harris <justin.harris@wpengine.com>
2014-12-11 17:44:44 -0600

    Adding bapi doc generation.

---
+++ src/main/scala/com/wordnik/swagger/codegen/BapiDocGenerator.scala
@@ -0,0 +1,16 @@
+package com.wordnik.swagger.codegen
+
+object BapiDocGenerator extends BapiDocGenerator {
+  def main(args: Array[String]) = generateClient(args)
+}
+
+class BapiDocGenerator extends SwaggerDocGenerator {
+
+  override def templateDir = "src/main/resources/bapi-static"
+  override def destinationDir = "docs/bapi/"
+
+  override def supportingFiles = List(
+    ("assets/images/logo.png", destinationDir + "/assets/images", "logo.png"),
+    ("index.mustache", destinationDir, "index.html")
+  )
+}
